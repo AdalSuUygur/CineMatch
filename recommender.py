d20902cf00e3716596cf016bfa757c43e0229620
Adal <adalsuuygur@gmail.com>
2026-01-27 15:25:33 +0300

    added files

---
+++ recommender.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+
+def get_recommendations(title, df):
+    # Türleri ve özetleri birleştirip matematiksel modele sokacağız
+    tfidf = TfidfVectorizer(stop_words='english')
+    tfidf_matrix = tfidf.fit_transform(df['genres']) # Şimdilik sadece türler
+    
+    # Cosine Similarity hesaplama
+    cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+    
+    # ... devamını beraber yazacağız ...
