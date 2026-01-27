b6eca62b084c515891604c83b677a1aa4a3bfb2b
Adal <adalsuuygur@gmail.com>
2026-01-28 02:36:09 +0300

    dosyayı bulmaya kadar halletim

--- backend/src/recommender.py
+++ backend/src/recommender.py
@@ -1,16 +1,39 @@
 
 #! Adal'ın algoritması
 
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import linear_kernel
+import pandas as pd #pd kısaltmasıyla pandas kütüphanesini çağırdık, pd genel kullanımda bu şekilde
+import os #OS Neydi? os emekti, şaka, OS meaning of Operating System.
 
-def get_recommendations(title, df):
-    # Türleri ve özetleri birleştirip matematiksel modele sokacağız
-    tfidf = TfidfVectorizer(stop_words='english')
-    tfidf_matrix = tfidf.fit_transform(df['genres']) # Şimdilik sadece türler
+class MovieRecommender:
+    def __init__(self, data_path: str):
+        """
+        Adal'ın Movie Recommender algoritması
+        
+        """
+        self.data_path = data_path
+        self.df = None
+
+    def load_data(self):
+        """
+        Bilgisayara "Dosya orada mı? diye soruyoruz.
+        Gerekli veri setini verir.
+        """
+        if os.path.exists(self.data_path):
+            self.df = pd.read_csv(self.data_path)
+            print("Dosya bulundu, yükleniyor...")
+        else:
+            print("Dosya bulunamadı!")
+
+# TEST BLOĞU
+if __name__ == "__main__":
+    # Bu satır, şu anki dosyanın (recommender.py) tam adresini bulur
+    current_dir = os.path.dirname(__file__)
     
-    # Cosine Similarity hesaplama
-    cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+    # Adresi şu anki dosyaya göre tarif ediyoruz:
+    # 'recommender.py' bir üst klasöre çık (..), 'data'ya gir, 'movies.csv'yi al
+    yol = os.path.abspath(os.path.join(current_dir, '..', 'data', 'movies.csv'))
     
-    # ... devamını beraber yazacağız ...
+    print(f"Aranan tam yol: {yol}") # Hangi adrese baktığını terminalde görelim
+    
+    adal_motoru = MovieRecommender(yol)
+    adal_motoru.load_data()
